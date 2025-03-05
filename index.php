653a78928273fc01fe5ab36bb7396c62dff8b204
Pedro Henrique <pedrohenriquegrazina22@gmail.com>
2025-03-05 13:34:49 -0300

    Criptografia

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
     } else {
 
         $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+        $senha = md5($mysqli->real_escape_string($_POST['senha']));
 
         $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
